f83cb60f19cccb703a0773eda3b63dc4f795a450
daiyun.chen <daiyun.chen@lie360.com>
2020-07-16 12:17:09 +0800

    ad

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import mixins from './mixins'
 import router from './router'
 import store from './store'
-
+Vue.mixin(mixins)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/mixins/index.js
@@ -0,0 +1,11 @@
+export default {
+  data() {
+    return {}
+  },
+  created() {
+    console.log('全局混乱引入成功')
+  },
+  methods: {
+
+  }
+}
